6194d799b3e5bb3675d23af9438b9a220a837bd8
teyycu12 <cch.hazell9@gmail.com>
2024-10-17 12:16:34 +0800

    Initial commit

---
+++ Assets/Scripts/DataNPC.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Hazel
+{
+    //<summary> 
+    //NPC資料 
+    //</summary> 
+    [CreateAssetMenu(menuName="KID/NPC")]
+    public class DataNPC
+    {
+        
+    }
+}
